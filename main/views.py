da91fb5446867607b95a8560797bf72bd44c0c02
IvanMSP <ivanlopz11@gmail.com>
2016-11-26 23:52:54 -0600

    Plantilla de Roxsoft

--- main/views.py
+++ main/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.views.generic import View
 
 class Home(View):
-	def get(self, request):
-			template_name= 'home.html'
-			return render(request, template_name)
+		def get(self, request):
+				template_name= 'home.html'
+				return render(request, template_name)
