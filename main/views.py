776051a66b9e10bf3723481838b24e10241b5f7d
Hugo <hugo-ensc@outlook.com>
2016-11-26 23:40:52 -0600

    Se arreglo views y settings

--- main/views.py
+++ main/views.py
@@ -3,5 +3,5 @@ from django.views.generic import View
 
 class Home(View):
 	def get(self, request):
-			template_name= 'home.html'
-			return render(request, template_name)
+		template_name= 'home.html'
+		return render(request, template_name)
